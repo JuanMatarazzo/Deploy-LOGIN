49d01a83ae5f1200d802c58cf827ee49cc420dfa
Juampa1998 <Juampa_junin@hotmail.com>
2024-08-02 16:46:49 -0300

    deploy back

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ const app = require("./src/app");
 const { database } = require("./src/db");
 require('dotenv').config()
 const {PORT} = process.env
+const port = process.env.PORT || 3000;
 
 database.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Escuchando en el`, PORT);
+  app.listen(port, () => {
+    console.log(`%s listening at`, port);
   });
 })
 .catch((err) => console.log(err.menssage))
